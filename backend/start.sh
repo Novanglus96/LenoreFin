a225bed405503a182bf0792bca77dcb9048abd0b
John Adams <johnmadams96@gmail.com>
2023-12-21 21:01:47 -0500

    chore: added fixtures to preload data

--- backend/start.sh
+++ backend/start.sh
@@ -5,13 +5,19 @@ python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 #python manage.py createcachetable
 
-if [ "$DJANGO_SUPERUSER_USERNAME" ]
-then
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
     (python manage.py createsuperuser \
         --noinput \
         --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL) \
-        || true
+        --email $DJANGO_SUPERUSER_EMAIL) ||
+        true
 fi
+
+python manage.py loaddata account_types
+python manage.py loaddata error_levels
+python manage.py loaddata options
+python manage.py loaddata repeats
+python manage.py loaddata transaction_statuses
+python manage.py loaddata transaction_types
 
 gunicorn backend.wsgi:application --bind 0.0.0.0:8000
