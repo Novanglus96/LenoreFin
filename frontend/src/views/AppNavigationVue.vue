3edee12921ad969c159a9c4d3cac500bb7d1fc33
John Adams <johnmadams96@gmail.com>
2024-01-03 11:42:38 -0500

    chore: prettied navigation bar

--- frontend/src/views/AppNavigationVue.vue
+++ frontend/src/views/AppNavigationVue.vue
@@ -1,219 +1,149 @@
 <template>
-    <v-app>
-        <v-container>
-            <v-app-bar color="primary">
-                <v-menu>
-                    <template v-slot:activator="{ props }">
-                        <v-btn prepend-icon="mdi-checkbook" v-bind="props">Checking</v-btn>
-                    </template>
+    <v-app-bar color="primary" density="compact">
+        <v-btn icon="mdi-view-dashboard" to="/"></v-btn>
+        <v-menu>
+            <template v-slot:activator="{ props }">
+                <v-btn prepend-icon="mdi-checkbook" v-bind="props">Checking</v-btn>
+            </template>
 
-                    <v-list>
-                        <v-list-item v-for="(account, i) in checking_accounts" :key="i">
-                            <v-list-item-title>{{ account.account_name }} </v-list-item-title>
-                            <v-list-item-subtitle>{{ account.balance }}</v-list-item-subtitle>
-                        </v-list-item>
-                        <v-divider></v-divider>
-                        <v-list-item prepend-icon="mdi-plus-circle" base-color="accent">
-                            <v-list-item-title>Add Account</v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-                <v-menu>
-                    <template v-slot:activator="{ props }">
-                        <v-btn prepend-icon="mdi-piggy-bank" v-bind="props">Savings</v-btn>
-                    </template>
+            <v-list>
+                <v-list-item title="No Accounts" v-if="checking_accounts.length == 0"></v-list-item>
+                <v-list-item v-for="(account, i) in checking_accounts" :key="i" :to="'/accounts/' + account.id" prepend-icon="mdi-checkbook" color="green" v-else>
+                    <v-list-item-title>{{ account.account_name }} </v-list-item-title>
+                    <v-list-item-subtitle><span :class="account.balance >= 0 ? 'text-green' : 'text-red'">${{ account.balance }}</span></v-list-item-subtitle>
+                </v-list-item>
+                <v-divider></v-divider>
+                <v-list-item prepend-icon="mdi-plus-circle" base-color="accent" :to="add_account_link">
+                    <v-list-item-title>Add Account</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+        <v-menu>
+            <template v-slot:activator="{ props }">
+                <v-btn prepend-icon="mdi-piggy-bank" v-bind="props">Savings</v-btn>
+            </template>
 
-                    <v-list>
-                        <v-list-item v-for="(account, i) in savings_accounts" :key="i">
-                            <v-list-item-title>{{ account.account_name }} </v-list-item-title>
-                            <v-list-item-subtitle>{{ account.balance }}</v-list-item-subtitle>
-                        </v-list-item>
-                        <v-divider></v-divider>
-                        <v-list-item prepend-icon="mdi-plus-circle" base-color="accent">
-                            <v-list-item-title>Add Account</v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-                <v-menu>
-                    <template v-slot:activator="{ props }">
-                        <v-btn prepend-icon="mdi-credit-card" v-bind="props">Credit Cards</v-btn>
-                    </template>
+            <v-list>
+                <v-list-item title="No Accounts" v-if="savings_accounts.length == 0"></v-list-item>
+                <v-list-item v-for="(account, i) in savings_accounts" :key="i" :to="'/accounts/' + account.id" prepend-icon="mdi-piggy-bank" v-else>
+                    <v-list-item-title>{{ account.account_name }} </v-list-item-title>
+                    <v-list-item-subtitle><span :class="account.balance >= 0 ? 'text-green' : 'text-red'">${{ account.balance }}</span></v-list-item-subtitle>
+                </v-list-item>
+                <v-divider></v-divider>
+                <v-list-item prepend-icon="mdi-plus-circle" base-color="accent" :to="add_account_link">
+                    <v-list-item-title>Add Account</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+        <v-menu>
+            <template v-slot:activator="{ props }">
+                <v-btn prepend-icon="mdi-credit-card" v-bind="props">Credit Cards</v-btn>
+            </template>
 
-                    <v-list>
-                        <v-list-item v-for="(account, i) in cc_accounts" :key="i">
-                            <v-list-item-title>{{ account.account_name }} </v-list-item-title>
-                            <v-list-item-subtitle>{{ account.balance }}</v-list-item-subtitle>
-                        </v-list-item>
-                        <v-divider></v-divider>
-                        <v-list-item prepend-icon="mdi-plus-circle" base-color="accent">
-                            <v-list-item-title>Add Account</v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-                <v-menu>
-                    <template v-slot:activator="{ props }">
-                        <v-btn prepend-icon="mdi-finance" v-bind="props">Investments</v-btn>
-                    </template>
+            <v-list>
+                <v-list-item title="No Accounts" v-if="cc_accounts.length == 0"></v-list-item>
+                <v-list-item v-for="(account, i) in cc_accounts" :key="i" :to="'/accounts/' + account.id" prepend-icon="mdi-credit-card" v-else>
+                    <v-list-item-title>{{ account.account_name }} </v-list-item-title>
+                    <v-list-item-subtitle><span :class="account.balance >= 0 ? 'text-green' : 'text-red'">${{ account.balance }}</span></v-list-item-subtitle>
+                </v-list-item>
+                <v-divider></v-divider>
+                <v-list-item prepend-icon="mdi-plus-circle" base-color="accent" :to="add_account_link">
+                    <v-list-item-title>Add Account</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+        <v-menu>
+            <template v-slot:activator="{ props }">
+                <v-btn prepend-icon="mdi-finance" v-bind="props">Investments</v-btn>
+            </template>
 
-                    <v-list>
-                        <v-list-item v-for="(account, i) in investment_accounts" :key="i">
-                            <v-list-item-title>{{ account.account_name }} </v-list-item-title>
-                            <v-list-item-subtitle>{{ account.balance }}</v-list-item-subtitle>
-                        </v-list-item>
-                        <v-divider></v-divider>
-                        <v-list-item prepend-icon="mdi-plus-circle" base-color="accent">
-                            <v-list-item-title>Add Account</v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-                <v-menu>
-                    <template v-slot:activator="{ props }">
-                        <v-btn prepend-icon="mdi-car-back" v-bind="props">Loans</v-btn>
-                    </template>
+            <v-list>
+                <v-list-item title="No Accounts" v-if="investment_accounts.length == 0"></v-list-item>
+                <v-list-item v-for="(account, i) in investment_accounts" :key="i" :to="'/accounts/' + account.id" prepend-icon="mdi-finance" v-else>
+                    <v-list-item-title>{{ account.account_name }} </v-list-item-title>
+                    <v-list-item-subtitle><span :class="account.balance >= 0 ? 'text-green' : 'text-red'">${{ account.balance }}</span></v-list-item-subtitle>
+                </v-list-item>
+                <v-divider></v-divider>
+                <v-list-item prepend-icon="mdi-plus-circle" base-color="accent" :to="add_account_link">
+                    <v-list-item-title>Add Account</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+        <v-menu>
+            <template v-slot:activator="{ props }">
+                <v-btn prepend-icon="mdi-car-back" v-bind="props">Loans</v-btn>
+            </template>
 
-                    <v-list>
-                        <v-list-item v-for="(account, i) in loan_accounts" :key="i">
-                            <v-list-item-title>{{ account.account_name }} </v-list-item-title>
-                            <v-list-item-subtitle>{{ account.balance }}</v-list-item-subtitle>
-                        </v-list-item>
-                        <v-divider></v-divider>
-                        <v-list-item prepend-icon="mdi-plus-circle" base-color="accent">
-                            <v-list-item-title>Add Account</v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-                <v-btn prepend-icon="mdi-chart-bar">Forecast</v-btn>
-                <v-btn prepend-icon="mdi-bell">Reminders</v-btn>
-                <v-menu>
-                    <template v-slot:activator="{ props }">
-                        <v-btn prepend-icon="mdi-folder" v-bind="props">Planning</v-btn>
-                    </template>
+            <v-list>
+                <v-list-item title="No Accounts" v-if="loan_accounts.length == 0"></v-list-item>
+                <v-list-item v-for="(account, i) in loan_accounts" :key="i" :to="'/accounts/' + account.id" prepend-icon="mdi-car-back" v-else>
+                    <v-list-item-title>{{ account.account_name }} </v-list-item-title>
+                    <v-list-item-subtitle><span :class="account.balance >= 0 ? 'text-green' : 'text-red'">${{ account.balance }}</span></v-list-item-subtitle>
+                </v-list-item>
+                <v-divider></v-divider>
+                <v-list-item prepend-icon="mdi-plus-circle" base-color="accent" :to="add_account_link">
+                    <v-list-item-title>Add Account</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+        <v-btn prepend-icon="mdi-chart-bar" to="/forecast">Forecast</v-btn>
+        <v-btn prepend-icon="mdi-bell" to="/reminders">Reminders</v-btn>
+        <v-menu>
+            <template v-slot:activator="{ props }">
+                <v-btn prepend-icon="mdi-folder" v-bind="props">Planning</v-btn>
+            </template>
 
-                    <v-list>
-                        <v-list-item v-for="(planning_item, i) in planning_menu" :key="i">
-                            <v-list-item-title>{{ planning_item.title }}</v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-                <v-btn prepend-icon="mdi-tag">Tags</v-btn>
-                <v-btn prepend-icon="mdi-cog">Settings</v-btn>
-                <v-spacer></v-spacer>
-                <v-btn icon="mdi-calculator"></v-btn>
-            </v-app-bar>
-        </v-container>
-    </v-app>
+            <v-list>
+                <v-list-item v-for="(planning_item, i) in planning_menu" :key="i" :prepend-icon="planning_item.icon" :to="planning_item.link">
+                    <v-list-item-title>{{ planning_item.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+        <v-btn prepend-icon="mdi-tag" to="/tags">Tags</v-btn>
+        <v-btn prepend-icon="mdi-cog" to="/settings">Settings</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn icon="mdi-calculator" to="/calculator"></v-btn>
+    </v-app-bar>
 </template>
 <script setup>
 import { ref } from 'vue'
-const checking_accounts = ref([
-    {
-        id: 1,
-        account_name: 'Checking Account # 1',
-        balance: "$1,000"
-    },
-    {
-        id: 2,
-        account_name: 'Checking Account # 2',
-        balance: "$1,100"
-    },
-    {
-        id: 3,
-        account_name: 'Checking Account # 3',
-        balance: "$1,200"
-    },
-])
-const savings_accounts = ref([
-    {
-        id: 1,
-        account_name: 'Savings Account # 1',
-        balance: "$2,000"
-    },
-    {
-        id: 2,
-        account_name: 'Savings Account # 2',
-        balance: "$2,100"
-    },
-    {
-        id: 3,
-        account_name: 'Savings Account # 3',
-        balance: "-$2,200"
-    },
-])
-const cc_accounts = ref([
-    {
-        id: 1,
-        account_name: 'Credit Card # 1',
-        balance: "$3,000"
-    },
-    {
-        id: 2,
-        account_name: 'Credit Card # 2',
-        balance: "$3,100"
-    },
-    {
-        id: 3,
-        account_name: 'Credit Card # 3',
-        balance: "-$3,200"
-    },
-])
-const investment_accounts = ref([
-    {
-        id: 1,
-        account_name: 'Investment Account # 1',
-        balance: "$4,000"
-    },
-    {
-        id: 2,
-        account_name: 'Investment Account # 2',
-        balance: "$4,100"
-    },
-    {
-        id: 3,
-        account_name: 'Investment Account # 3',
-        balance: "-$4,200"
-    },
-])
-const loan_accounts = ref([
-    {
-        id: 1,
-        account_name: 'Loan Account # 1',
-        balance: "$5,000"
-    },
-    {
-        id: 2,
-        account_name: 'Loan Account # 2',
-        balance: "$5,100"
-    },
-    {
-        id: 3,
-        account_name: 'Loan Account # 3',
-        balance: "-$5,200"
-    },
-])
+
+const add_account_link = ref("/accounts/add")
+const checking_accounts = ref([])
+const savings_accounts = ref([])
+const cc_accounts = ref([])
+const investment_accounts = ref([])
+const loan_accounts = ref([])
 const planning_menu = ref([
     {
         title: 'Pay',
-        link: "/"
+        link: "/planning/pay",
+        icon: "mdi-checkbook"
     },
     {
         title: 'Expenses',
-        link: "/"
+        link: "/planning/expenses",
+        icon: "mdi-cash"
     },
     {
         title: 'Contributions',
-        link: "/"
+        link: "/planning/contributions",
+        icon: "mdi-pail"
     },
     {
         title: 'Retirement',
-        link: "/"
+        link: "/planning/retirement",
+        icon: "mdi-piggy-bank"
     },
     {
         title: 'Christmas',
-        link: "/"
+        link: "/planning/christmas",
+        icon: "mdi-pine-tree"
     },
     {
         title: 'Notes',
-        link: "/"
+        link: "/planning/notes",
+        icon: "mdi-note"
     },
 ])
 </script>
--- frontend/src/views/DashBoard.vue
+++ frontend/src/views/DashBoard.vue
@@ -1,5 +1,18 @@
 <template>
     <div>
-        
+        <v-row class="bg-accent pa-1 ga-1">
+            <v-col class="bg-white rounded">
+                Main Expenses <br />
+                New Content
+            </v-col>
+            <v-col class="bg-white rounded"> Kids Expenses </v-col>
+            <v-col class="bg-white rounded"> Money Pile Expenses </v-col>
+        </v-row>
+        <v-row class="bg-accent pa-1 ga-1 rounded">
+            <v-col class="bg-white rounded"> Upcoming Reminders </v-col>
+        </v-row>
+        <v-row class="bg-accent pa-1 ga-1 rounded">
+            <v-col class="bg-white rounded"> Upcoming Transactions </v-col>
+        </v-row>
     </div>
 </template>
