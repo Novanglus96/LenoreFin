4989be941b9d9485cd32ae56d0aa0561db15d184
John Adams <johnmadams96@gmail.com>
2024-01-03 16:18:04 -0500

    chore: add placeholder widget for Main Expenses

---
+++ frontend/src/components/MainExpensesWidget.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-container class="ma-0 pa-0 ga-0">
+        <v-row dense>
+            <v-col><v-span class="text-subtitle-2 text-accent">Main Expenses</v-span></v-col><v-col cols=1 class="text-right"><v-btn icon="mdi-cog" size="xs" flat></v-btn></v-col>
+        </v-row>
+        <v-row dense>
+            <v-col>
+                <Pie :data="data" :options="options" />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup>
+import { Pie } from 'vue-chartjs'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { ref } from 'vue'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+const data = ref({
+    labels: [
+        'Money Pile',
+        'Kids',
+        'Rent',
+        'Subscriptions'
+    ],
+    datasets: [{
+        label: 'Main Expenses',
+        data: [.33, 58.05, 36.04, 5.58],
+        backgroundColor: [
+            '#06966A',
+            '#ECFDF5',
+            'green',
+            'blue',
+        ],
+        hoverOffset: 4
+    }]
+})
+const options = ref({
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+        legend: {
+            position: 'right'
+        }
+    }
+})
+
+</script>
