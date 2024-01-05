cfc16bb5fada6c17eb70a646186d3e31aabf0f47
John Adams <johnmadams96@gmail.com>
2024-01-05 09:18:17 -0500

    chore: add tanstack vue-query

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,18 +2,20 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import piniaPluginPersistedState from "pinia-plugin-persistedstate"
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import { VueQueryPlugin } from "@tanstack/vue-query"
 
 loadFonts()
 
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
 const app = createApp(App)
 
 app.use(router)
-app.use(store)
 app.use(pinia)
 app.use(vuetify)
+app.use(VueQueryPlugin)
 app.mount('#app')
   
