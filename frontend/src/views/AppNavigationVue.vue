482dd3190c7b7b57a82f136dbb5f4b040cc343cb
John Adams <johnmadams96@gmail.com>
2024-01-05 15:44:53 -0500

    chore: centralize accounts/planning menus

--- frontend/src/views/AppNavigationVue.vue
+++ frontend/src/views/AppNavigationVue.vue
@@ -37,108 +37,18 @@
         </v-navigation-drawer>
 
         <v-navigation-drawer permanent widht="250" color="primary" v-if="mdAndUp">
-        <v-list density="compact" nav v-if="nav_toggle">
-            <v-list-item prepend-icon="mdi-plus-circle" base-color="accent" :to="add_account_link">
-                <v-list-item-title>Add Account</v-list-item-title>
-            </v-list-item>
-        </v-list>
-        <v-list density="compact" nav v-if="nav_toggle">
-            <v-list-subheader><v-icon icon="mdi-checkbook"></v-icon> CHECKING</v-list-subheader>
-            <v-list-item title="No Accounts" v-if="checking_accounts.length == 0"></v-list-item>
-            <v-list-item v-for="(account, i) in checking_accounts" :key="i" :to="'/accounts/' + account.id" v-else>
-                <v-list-item-title>{{ account.account_name }} </v-list-item-title>
-                <v-list-item-subtitle><span :class="account.balance >= 0 ? 'text-green font-weight-bold' : 'text-red font-weight-bold'">${{ account.balance }}</span></v-list-item-subtitle>
-            </v-list-item>
-        </v-list>
-        <v-divider></v-divider>
-        <v-list density="compact" nav v-if="nav_toggle">
-            <v-list-subheader><v-icon icon="mdi-piggy-bank"></v-icon> SAVINGS</v-list-subheader>
-            <v-list-item title="No Accounts" v-if="savings_accounts.length == 0"></v-list-item>
-            <v-list-item v-for="(account, i) in savings_accounts" :key="i" :to="'/accounts/' + account.id" v-else>
-                <v-list-item-title>{{ account.account_name }} </v-list-item-title>
-                <v-list-item-subtitle><span :class="account.balance >= 0 ? 'text-green font-weight-bold' : 'text-red font-weight-bold'">${{ account.balance }}</span></v-list-item-subtitle>
-            </v-list-item>
-        </v-list>
-        <v-divider></v-divider>
-        <v-list density="compact" nav v-if="nav_toggle">
-            <v-list-subheader><v-icon icon="mdi-credit-card"></v-icon> CREDIT CARD</v-list-subheader>
-            <v-list-item title="No Accounts" v-if="cc_accounts.length == 0"></v-list-item>
-            <v-list-item v-for="(account, i) in cc_accounts" :key="i" :to="'/accounts/' + account.id" v-else>
-                <v-list-item-title>{{ account.account_name }} </v-list-item-title>
-                <v-list-item-subtitle><span :class="account.balance >= 0 ? 'text-green font-weight-bold' : 'text-red font-weight-bold'">${{ account.balance }}</span></v-list-item-subtitle>
-            </v-list-item>
-        </v-list>
-        <v-divider></v-divider>
-        <v-list density="compact" nav v-if="nav_toggle">
-            <v-list-subheader><v-icon icon="mdi-finance"></v-icon> INVESTMENT</v-list-subheader>
-            <v-list-item title="No Accounts" v-if="investment_accounts.length == 0"></v-list-item>
-            <v-list-item v-for="(account, i) in investment_accounts" :key="i" :to="'/accounts/' + account.id" v-else>
-                <v-list-item-title>{{ account.account_name }} </v-list-item-title>
-                <v-list-item-subtitle><span :class="account.balance >= 0 ? 'text-green font-weight-bold' : 'text-red font-weight-bold'">${{ account.balance }}</span></v-list-item-subtitle>
-            </v-list-item>
-        </v-list>
-        <v-divider></v-divider>
-        <v-list density="compact" nav v-if="nav_toggle">
-            <v-list-subheader><v-icon icon="mdi-car-back"></v-icon> LOAN</v-list-subheader>
-            <v-list-item title="No Accounts" v-if="loan_accounts.length == 0"></v-list-item>
-            <v-list-item v-for="(account, i) in loan_accounts" :key="i" :to="'/accounts/' + account.id" v-else>
-                <v-list-item-title>{{ account.account_name }} </v-list-item-title>
-                <v-list-item-subtitle><span :class="account.balance >= 0 ? 'text-green font-weight-bold' : 'text-red font-weight-bold'">${{ account.balance }}</span></v-list-item-subtitle>
-            </v-list-item>
-        </v-list>
-        <v-list density="compact" nav v-if="!nav_toggle">
-            <v-list-subheader><v-icon icon="mdi-folder"></v-icon> PLANNING</v-list-subheader>
-            <v-list-item v-for="(planning_item, i) in planning_menu" :key="i" :prepend-icon="planning_item.icon" :to="planning_item.link">
-                <v-list-item-title>{{ planning_item.title }}</v-list-item-title>
-            </v-list-item>
-        </v-list>
+        <AccountsMenu v-if="nav_toggle"/>
+        <PlanningMenu v-if="!nav_toggle"/>
     </v-navigation-drawer>
 </div>
 </template>
 <script setup>
 import { ref } from 'vue'
-import { useAccounts } from '@/composables/accountsComposable'
 import { useDisplay } from 'vuetify'
+import AccountsMenu from '@/components/AccountsMenu.vue'
+import PlanningMenu from '@/components/PlanningMenu.vue'
 
 const { mdAndUp } = useDisplay()
 const nav_toggle = ref(true)
-const { checking_accounts, cc_accounts, savings_accounts, investment_accounts, loan_accounts } = useAccounts()
-const add_account_link = ref("/accounts/add")
-const planning_menu = ref([
-    {
-        title: 'Pay',
-        link: "/planning/pay",
-        icon: "mdi-checkbook"
-    },
-    {
-        title: 'Expenses',
-        link: "/planning/expenses",
-        icon: "mdi-cash"
-    },
-    {
-        title: 'Contributions',
-        link: "/planning/contributions",
-        icon: "mdi-pail"
-    },
-    {
-        title: 'Retirement',
-        link: "/planning/retirement",
-        icon: "mdi-piggy-bank"
-    },
-    {
-        title: 'Christmas',
-        link: "/planning/christmas",
-        icon: "mdi-pine-tree"
-    },
-    {
-        title: 'Notes',
-        link: "/planning/notes",
-        icon: "mdi-note"
-    },
-    {
-        title: 'Calculator',
-        link: "/planning/calculator",
-        icon: "mdi-calculator"
-    },
-])
+
 </script>
