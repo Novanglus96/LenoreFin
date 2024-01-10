5d8c07e3ad4546fc8c1eca36e20c7a694deb7e9c
John Adams <johnmadams96@gmail.com>
2024-01-10 11:05:54 -0500

    chore: add api for account types

---
+++ frontend/src/composables/accountTypesComposable.js
@@ -0,0 +1,54 @@
+import { useQuery, useQueryClient } from "@tanstack/vue-query";
+import axios from 'axios'
+import { useMainStore } from '@/stores/main'
+
+const apiClient = axios.create({
+  baseURL: '/api/v1',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer sVruPBzWnGEDrLb7JjfVNrs9wk8LtgnDQef6iXDXc4bWMUk3XFcsCtEgT8dKzhJd' //TODO: Pull API_KEY from somewhere secure
+  }
+})
+
+function handleApiError(error, message) {
+  const mainstore = useMainStore();
+  if (error.response) {
+    console.error('Response error:', error.response.data)
+    console.error('Status code:', error.response.status)
+    console.error('Headers', error.response.headers)
+  } else if (error.request){
+    console.error('No response received:', error.request)
+  } else {
+    console.error('Error during request setup:', error.message)
+  }
+  mainstore.showSnackbar(message + 'Error #' + error.response.status, 'error')
+  throw error
+}
+
+async function getAccountTypesFunction() {
+  try {
+    const response = await apiClient.get('/accounts/types')
+    return response.data
+      
+    } catch (error) {
+      handleApiError(error, 'Account types not fetched: ')
+    }
+
+}
+
+export function useAccountTypes() {
+  const queryClient = useQueryClient()
+  const { data: account_types, isLoading } = useQuery({
+    queryKey: ['account_types'],
+    queryFn: () => getAccountTypesFunction(),
+    select: (response) => response,
+    client: queryClient
+  })
+  
+  return {
+    isLoading,
+    account_types
+  }
+}
