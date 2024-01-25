b2e399d5210f2bad16ccbede727ec968cf6696d9
John Adams <johnmadams96@gmail.com>
2024-01-25 14:29:50 -0500

    chore: more skeleton work for tag view

--- frontend/src/components/TagsHeaderWidget.vue
+++ frontend/src/components/TagsHeaderWidget.vue
@@ -21,40 +21,26 @@
                             show-arrows
                             center-active
                         >
-                        <v-slide-group-item
-                            v-for="tag in tags"
-                            :key="tag.id"
-                            v-slot="{ toggle, selectedClass }"
-                            @group:selected="clickSelectTag"
-                            :value="tag.id"
-                        >
-                            <v-card
-                            color="primary"
-                            :class="['ma-4', selectedClass]"
-                            height="75"
-                            width="200"
-                            @click="toggle"
-                            :title="tag.parent ? tag.parent.tag_name : tag.tag_name"
-                            :subtitle="!tag.parent ? '' : tag.tag_name"
-                            prepend-icon="mdi-tag"
+                            <v-slide-group-item
+                                v-for="tag in tags"
+                                :key="tag.id"
+                                v-slot="{ toggle, selectedClass }"
+                                @group:selected="clickSelectTag"
+                                :value="tag.id"
                             >
-                            </v-card>
-                        </v-slide-group-item>
+                                <v-card
+                                color="primary"
+                                :class="['ma-4', selectedClass]"
+                                height="75"
+                                width="200"
+                                @click="toggle"
+                                :title="tag.parent ? tag.parent.tag_name : tag.tag_name"
+                                :subtitle="!tag.parent ? '' : tag.tag_name"
+                                prepend-icon="mdi-tag"
+                                >
+                                </v-card>
+                            </v-slide-group-item>
                         </v-slide-group>
-
-                        <v-expand-transition>
-                        <v-sheet
-                            v-if="tag_selected != null"
-                            height="150"
-                            color="primary"
-                        >
-                            <div class="d-flex fill-height align-center justify-center">
-                            <h3 class="text-h6">
-                                Selected {{ tag_selected }}
-                            </h3>
-                            </div>
-                        </v-sheet>
-                        </v-expand-transition>
                     </v-col>
                 </v-row>
             </template>
--- frontend/src/views/TagsView.vue
+++ frontend/src/views/TagsView.vue
@@ -2,17 +2,28 @@
     <div>
         <v-row class="pa-1 ga-1" no-gutters>
             <v-col class="rounded">
-                <TagsHeaderWidget />
+                <TagsHeaderWidget @tag-selected="clickSelectedTag"/>
             </v-col>
         </v-row>
         <v-row class="pa-1 ga-1 rounded" no-gutters>
             <v-col class="rounded">
-                Transactions
+                Averages {{ selected_tag }}
+            </v-col>
+        </v-row>
+        <v-row class="pa-1 ga-1 rounded" no-gutters>
+            <v-col class="rounded">
+                Transactions {{ selected_tag }}
             </v-col>
         </v-row>
     </div>
 </template>
 <script setup>
 import TagsHeaderWidget from '@/components/TagsHeaderWidget.vue'
+import { ref } from 'vue'
 
+const selected_tag = ref(null)
+
+const clickSelectedTag = (tag_id) => {
+    selected_tag.value = tag_id
+}
 </script>
