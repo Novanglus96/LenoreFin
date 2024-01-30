2003b2a064578d6d405789aafbe7edc85c9bb63d
John Adams <johnmadams96@gmail.com>
2024-01-30 14:13:26 -0500

    chore: placeholder for field settings

---
+++ frontend/src/components/FieldsWidget.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-card>
+        <v-card-title>Fields</v-card-title>
+        <v-card-actions><v-btn>Save</v-btn></v-card-actions>
+    </v-card>
+</template>
+<script setup>
+</script>
