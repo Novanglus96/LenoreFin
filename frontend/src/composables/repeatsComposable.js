4dd23804e927074e593ed13e2af286bd78ddef89
John Adams <johnmadams96@gmail.com>
2024-02-12 16:24:23 -0500

    chore: add repeats composable

---
+++ frontend/src/composables/repeatsComposable.js
@@ -0,0 +1,57 @@
+import { useQuery, useQueryClient } from "@tanstack/vue-query";
+import axios from 'axios'
+import { useMainStore } from '@/stores/main'
+import { logToDB } from "./logentriesComposable"
+
+const apiClient = axios.create({
+  baseURL: '/api/v1',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer sVruPBzWnGEDrLb7JjfVNrs9wk8LtgnDQef6iXDXc4bWMUk3XFcsCtEgT8dKzhJd' //TODO: Pull API_KEY from somewhere secure
+  }
+})
+
+function handleApiError(error, message) {
+  const mainstore = useMainStore();
+  if (error.response) {
+    console.error('Response error:', error.response.data)
+    console.error('Status code:', error.response.status)
+    console.error('Headers', error.response.headers)
+  } else if (error.request){
+    console.error('No response received:', error.request)
+  } else {
+    console.error('Error during request setup:', error.message)
+  }
+  mainstore.showSnackbar(message + 'Error #' + error.response.status, 'error')
+  throw error
+}
+
+async function getRepeatsFunction() {
+  try {
+    const response = await apiClient.get('/reminders/repeats')
+    logToDB(null, 'Repeats fetched', 0, null, null, null)
+    return response.data
+      
+  } catch (error) {
+    handleApiError(error, 'Repeats not fetched: ')
+    logToDB(error, 'Repeats not fetched', 2, null, null, null)
+  }
+
+}
+
+export function useRepeats() {
+  const queryClient = useQueryClient()
+  const { data: repeats, isLoading } = useQuery({
+    queryKey: ['repeats'],
+    queryFn: () => getRepeatsFunction(),
+    select: (response) => response,
+    client: queryClient
+})
+
+return {
+  isLoading,
+  repeats
+}
+}
