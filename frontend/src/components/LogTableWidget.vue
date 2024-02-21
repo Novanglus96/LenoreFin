0addbb3c62de50e77acdb0a10d0da1541be39d06
John Adams <johnmadams96@gmail.com>
2024-02-21 16:15:56 -0500

    chore: reformat log table

--- frontend/src/components/LogTableWidget.vue
+++ frontend/src/components/LogTableWidget.vue
@@ -19,6 +19,9 @@
       paginationInfo="Showing {0} to {1} of {2} log entries"
       class="alt-pagination"
     >
+      <template #log_date="row">
+        {{ getPrettyDate(row.value.log_date) }}
+      </template>
       <template #error_level="row">
         <v-btn v-if="row.value.error_level.id == 0" variant="plain">
           <v-icon icon="mdi-bug" color="blue"></v-icon>
@@ -65,12 +68,18 @@ import { ref } from "vue";
 const { log_entries, isLoading } = useLogEntries();
 
 const columns = ref([
-  { field: "log_date", title: "Date", width: "120px" },
-  { field: "error_level", title: "Level", width: "50px" },
-  { field: "log_entry", title: "Entry" },
+  { field: "error_level", title: "", width: "25px" },
+  { field: "log_date", title: "Date", width: "175px" },
   { field: "error_num", title: "Error #", type: "number", width: "100px" },
-  { field: "account.account_name", title: "Account" },
-  { field: "reminder.description", title: "Reminder" },
-  { field: "transaction.description", title: "Transaction" },
+  { field: "log_entry", title: "Entry" },
+  { field: "account.account_name", title: "Account", width: "150px" },
+  { field: "reminder.description", title: "Reminder", width: "150px" },
+  { field: "transaction.description", title: "Transaction", width: "150px" },
 ]);
+
+const getPrettyDate = uglyDate => {
+  const newDate = new Date(uglyDate);
+  const formattedDate = newDate.toLocaleString("en-US");
+  return formattedDate;
+};
 </script>
