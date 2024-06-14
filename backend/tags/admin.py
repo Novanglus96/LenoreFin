2515a4e69721f541b13dd0557f64d79ed96bef06
John Adams <johnmadams96@gmail.com>
2024-06-14 14:45:51 -0400

    chore: update admin for tags app

--- backend/tags/admin.py
+++ backend/tags/admin.py
@@ -5,5 +5,26 @@ from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
-admin.site.register(Tag)
-admin.site.register(TagType)
+
+class TagAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "tag_name", "parent", "tag_type"]
+
+    list_display_links = ["tag_name"]
+
+    ordering = ["parent", "tag_name"]
+
+    list_filter = ["parent", "tag_type"]
+
+    search_fields = ["tag_name"]
+
+
+class TagTypeAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "tag_type"]
+
+    list_display_links = ["tag_type"]
+
+    ordering = ["id"]
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(TagType, TagTypeAdmin)
