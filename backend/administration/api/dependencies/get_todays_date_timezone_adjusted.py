77ac65c42ac695047a92034e127094f19e2e8f62
John Adams <johnmadams96@gmail.com>
2024-07-31 10:40:50 -0400

    chore: added time option to get_todays_date_timezone_adjusted

--- backend/administration/api/dependencies/get_todays_date_timezone_adjusted.py
+++ backend/administration/api/dependencies/get_todays_date_timezone_adjusted.py
@@ -4,17 +4,23 @@ import pytz
 import os
 
 
-def get_todays_date_timezone_adjusted() -> date:
+def get_todays_date_timezone_adjusted(get_time: bool = False) -> date:
     """
-    The function `get_todays_date_timezone_adjusted` returns todays date
-    adjusted for the local timezone of the app.
+    The function `get_todays_date_timezone_adjusted` returns today's date
+    adjusted for the local timezone of the app. If `get_time` is set to True,
+    it returns the current time instead.
 
     Args:
+        get_time (bool): If True, returns the current time instead of the date. Defaults to False.
 
     Returns:
-        date: todays date
+        date or datetime: Today's date or current time adjusted for the local timezone.
     """
     today = timezone.now()
-    tz_timezone = pytz.timezone(os.environ.get("TIMEZONE"))
-    today_tz = today.astimezone(tz_timezone).date()
-    return today_tz
+    tz_timezone = pytz.timezone(os.environ.get("TIMEZONE", "UTC"))
+    today_tz = today.astimezone(tz_timezone)
+
+    if get_time:
+        return today_tz
+    else:
+        return today_tz.date()
