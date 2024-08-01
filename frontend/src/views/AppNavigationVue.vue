461f53ca00bb731115a14d23ba1980de8399c442
John Adams <johnmadams96@gmail.com>
2024-08-01 15:17:40 -0400

    chore: changed version to 1.0.3

--- frontend/src/views/AppNavigationVue.vue
+++ frontend/src/views/AppNavigationVue.vue
@@ -6,7 +6,7 @@
       </template>
       <v-app-bar-title>
         <span class="text-caption font-weight-bold"
-          >v1.0.2</span
+          >v1.0.3</span
         ></v-app-bar-title
       >
       <v-menu location="start">
