5c5c7fa7a2cd6d81a26c2c5e58046c28e4aa376f
John Adams <johnmadams96@gmail.com>
2024-08-07 11:10:26 -0400

    fix: typo in version number

--- frontend/src/views/AppNavigationVue.vue
+++ frontend/src/views/AppNavigationVue.vue
@@ -6,7 +6,7 @@
       </template>
       <v-app-bar-title>
         <span class="text-caption font-weight-bold"
-          >v1.0.11</span
+          >v1.0.011</span
         ></v-app-bar-title
       >
       <v-menu location="start">
