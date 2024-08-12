bd9d483abd88f384466fdfc0faac393d725c32cd
John Adams <johnmadams96@gmail.com>
2024-08-12 15:27:30 -0400

    fix: default tab set for transaction form

--- frontend/src/components/TransactionForm.vue
+++ frontend/src/components/TransactionForm.vue
@@ -578,7 +578,7 @@ const tagComplete = ref(false); // True when tag/amount are filled out, enables 
 const formComplete = ref(false); // True when form is validated, enables add/update button
 const selected = ref([]); // The objects of the rows selected in table
 const details_table = ref(null); // The reference to the table
-const tab = ref(null); // Tab model
+const tab = ref(0); // Tab model
 const isPaycheck = ref(null); // True if this transaction a paycheck
 const paycheckTotalsMatch = ref(false); // True if the paycheck fields total = gross
 
@@ -1011,7 +1011,7 @@ const closeDialog = () => {
   } else {
     isPaycheck.value = false;
   }
-  tab.value = null;
+  tab.value = 0;
   formComplete.value = false;
   emit("updateDialog", false);
 };
