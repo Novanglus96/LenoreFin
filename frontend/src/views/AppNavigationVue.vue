29d8cc47ac5bfdd498a21bf8cee3e76bfba7048e
John Adams <johnmadams96@gmail.com>
2024-08-30 12:55:34 -0400

    chore: change to version 1.0.025

--- frontend/src/views/AppNavigationVue.vue
+++ frontend/src/views/AppNavigationVue.vue
@@ -6,7 +6,7 @@
       </template>
       <v-app-bar-title>
         <span class="text-caption font-weight-bold"
-          >v1.0.024</span
+          >v1.0.025</span
         ></v-app-bar-title
       >
       <v-menu location="start">
