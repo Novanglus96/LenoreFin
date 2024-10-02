788e27e6e7cb3133d508eb7aee6934fb6460e7f4
John Adams <johnmadams96@gmail.com>
2024-10-02 16:05:22 -0400

    chore: change to version 1.0.033

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -52,7 +52,7 @@ const { prefetchVersion, version } = useVersion();
 const showBanner = ref(false);
 
 const checkVersion = computed(() => {
-  return version.value && version.value.version_number !== "1.0.032";
+  return version.value && version.value.version_number !== "1.0.033";
 });
 
 const updateBanner = () => {
