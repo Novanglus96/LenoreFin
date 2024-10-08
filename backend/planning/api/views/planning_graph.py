a44d55e3f0bf2683b19f1422477bd62b88ba77b4
John Adams <johnmadams96@gmail.com>
2024-10-08 11:45:01 -0400

    chore: api change to graph helper function

--- backend/planning/api/views/planning_graph.py
+++ backend/planning/api/views/planning_graph.py
@@ -438,173 +438,6 @@ def list_graph_totals(request, graph_type: str):
                         output_field=FloatField(),
                     ),
                 )
-                # Prepare the datasets
-                datasets = []
-                this_year_dataset = DatasetObject(
-                    label=this_year,
-                    backgroundColor="#046959",
-                    data=[
-                        abs(
-                            sum(
-                                item.total_jan_current
-                                for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_feb_current
-                                for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_mar_current
-                                for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_apr_current
-                                for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_may_current
-                                for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_jun_current
-                                for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_jul_current
-                                for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_aug_current
-                                for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_sep_current
-                                for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_oct_current
-                                for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_nov_current
-                                for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_dec_current
-                                for item in tag_group_totals
-                            )
-                        ),
-                    ],
-                )
-                datasets.append(this_year_dataset)
-                last_year_dataset = DatasetObject(
-                    label=last_year,
-                    backgroundColor="#c2fff5",
-                    data=[
-                        abs(
-                            sum(
-                                item.total_jan_last for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_feb_last for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_mar_last for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_apr_last for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_may_last for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_jun_last for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_jul_last for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_aug_last for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_sep_last for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_oct_last for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_nov_last for item in tag_group_totals
-                            )
-                        ),
-                        abs(
-                            sum(
-                                item.total_dec_last for item in tag_group_totals
-                            )
-                        ),
-                    ],
-                )
-                datasets.append(last_year_dataset)
-
-                # Prepare the GraphData object
-                graph_data = GraphData(
-                    labels=[
-                        "January",
-                        "February",
-                        "March",
-                        "April",
-                        "May",
-                        "June",
-                        "July",
-                        "August",
-                        "September",
-                        "October",
-                        "November",
-                        "December",
-                    ],
-                    datasets=datasets,
-                )
 
                 # Calculate averages
                 current_avg = abs(
@@ -615,17 +448,68 @@ def list_graph_totals(request, graph_type: str):
                     sum(item.total_last_year for item in tag_group_totals) / 12
                 )
 
-                # Prepare the planning graph out object
-                tag_graph_out = PlanningGraphOut(
-                    data=graph_data,
-                    year1=this_year,
-                    year2=last_year,
-                    year1_avg=current_avg,
-                    year2_avg=last_avg,
-                    pretty_name=pretty_name,
-                    key_name=key_name,
+                this_year_data = [
+                    abs(
+                        sum(item.total_jan_current for item in tag_group_totals)
+                    ),
+                    abs(
+                        sum(item.total_feb_current for item in tag_group_totals)
+                    ),
+                    abs(
+                        sum(item.total_mar_current for item in tag_group_totals)
+                    ),
+                    abs(
+                        sum(item.total_apr_current for item in tag_group_totals)
+                    ),
+                    abs(
+                        sum(item.total_may_current for item in tag_group_totals)
+                    ),
+                    abs(
+                        sum(item.total_jun_current for item in tag_group_totals)
+                    ),
+                    abs(
+                        sum(item.total_jul_current for item in tag_group_totals)
+                    ),
+                    abs(
+                        sum(item.total_aug_current for item in tag_group_totals)
+                    ),
+                    abs(
+                        sum(item.total_sep_current for item in tag_group_totals)
+                    ),
+                    abs(
+                        sum(item.total_oct_current for item in tag_group_totals)
+                    ),
+                    abs(
+                        sum(item.total_nov_current for item in tag_group_totals)
+                    ),
+                    abs(
+                        sum(item.total_dec_current for item in tag_group_totals)
+                    ),
+                    current_avg,
+                ]
+                last_year_data = [
+                    abs(sum(item.total_jan_last for item in tag_group_totals)),
+                    abs(sum(item.total_feb_last for item in tag_group_totals)),
+                    abs(sum(item.total_mar_last for item in tag_group_totals)),
+                    abs(sum(item.total_apr_last for item in tag_group_totals)),
+                    abs(sum(item.total_may_last for item in tag_group_totals)),
+                    abs(sum(item.total_jun_last for item in tag_group_totals)),
+                    abs(sum(item.total_jul_last for item in tag_group_totals)),
+                    abs(sum(item.total_aug_last for item in tag_group_totals)),
+                    abs(sum(item.total_sep_last for item in tag_group_totals)),
+                    abs(sum(item.total_oct_last for item in tag_group_totals)),
+                    abs(sum(item.total_nov_last for item in tag_group_totals)),
+                    abs(sum(item.total_dec_last for item in tag_group_totals)),
+                    last_avg,
+                ]
+                planning_graph_out = prepare_planning_graph(
+                    pretty_name,
+                    this_year_data,
+                    last_year_data,
+                    this_year,
+                    last_year,
                 )
-                sub_graph_data.append(tag_graph_out)
+                sub_graph_data.append(planning_graph_out)
             graph_object = PlanningGraphList(title=title, data=sub_graph_data)
             all_reports.append(graph_object)
 
@@ -1039,79 +923,45 @@ def list_graph_totals(request, graph_type: str):
                 )
                 for sub_tag in transactions_by_tag:
                     pretty_name = sub_tag.pretty_name
-                    key_name = pretty_name.replace(" ", "_").lower()
+                    this_year_data = [
+                        abs(sub_tag.total_jan_current),
+                        abs(sub_tag.total_feb_current),
+                        abs(sub_tag.total_mar_current),
+                        abs(sub_tag.total_apr_current),
+                        abs(sub_tag.total_may_current),
+                        abs(sub_tag.total_jun_current),
+                        abs(sub_tag.total_jul_current),
+                        abs(sub_tag.total_aug_current),
+                        abs(sub_tag.total_sep_current),
+                        abs(sub_tag.total_oct_current),
+                        abs(sub_tag.total_nov_current),
+                        abs(sub_tag.total_dec_current),
+                        abs(sub_tag.current_year_avg),
+                    ]
+                    last_year_data = [
+                        abs(sub_tag.total_jan_last),
+                        abs(sub_tag.total_feb_last),
+                        abs(sub_tag.total_mar_last),
+                        abs(sub_tag.total_apr_last),
+                        abs(sub_tag.total_may_last),
+                        abs(sub_tag.total_jun_last),
+                        abs(sub_tag.total_jul_last),
+                        abs(sub_tag.total_aug_last),
+                        abs(sub_tag.total_sep_last),
+                        abs(sub_tag.total_oct_last),
+                        abs(sub_tag.total_nov_last),
+                        abs(sub_tag.total_dec_last),
+                        abs(sub_tag.last_year_avg),
+                    ]
 
-                    # Prepare the datasets
-                    datasets = []
-                    this_year_dataset = DatasetObject(
-                        label=this_year,
-                        backgroundColor="#046959",
-                        data=[
-                            abs(sub_tag.total_jan_current),
-                            abs(sub_tag.total_feb_current),
-                            abs(sub_tag.total_mar_current),
-                            abs(sub_tag.total_apr_current),
-                            abs(sub_tag.total_may_current),
-                            abs(sub_tag.total_jun_current),
-                            abs(sub_tag.total_jul_current),
-                            abs(sub_tag.total_aug_current),
-                            abs(sub_tag.total_sep_current),
-                            abs(sub_tag.total_oct_current),
-                            abs(sub_tag.total_nov_current),
-                            abs(sub_tag.total_dec_current),
-                        ],
+                    planning_graph_out = prepare_planning_graph(
+                        pretty_name,
+                        this_year_data,
+                        last_year_data,
+                        this_year,
+                        last_year,
                     )
-                    datasets.append(this_year_dataset)
-                    last_year_dataset = DatasetObject(
-                        label=last_year,
-                        backgroundColor="#c2fff5",
-                        data=[
-                            abs(sub_tag.total_jan_last),
-                            abs(sub_tag.total_feb_last),
-                            abs(sub_tag.total_mar_last),
-                            abs(sub_tag.total_apr_last),
-                            abs(sub_tag.total_may_last),
-                            abs(sub_tag.total_jun_last),
-                            abs(sub_tag.total_jul_last),
-                            abs(sub_tag.total_aug_last),
-                            abs(sub_tag.total_sep_last),
-                            abs(sub_tag.total_oct_last),
-                            abs(sub_tag.total_nov_last),
-                            abs(sub_tag.total_dec_last),
-                        ],
-                    )
-                    datasets.append(last_year_dataset)
-
-                    # Prepare the GraphData object
-                    graph_data = GraphData(
-                        labels=[
-                            "January",
-                            "February",
-                            "March",
-                            "April",
-                            "May",
-                            "June",
-                            "July",
-                            "August",
-                            "September",
-                            "October",
-                            "November",
-                            "December",
-                        ],
-                        datasets=datasets,
-                    )
-
-                    # Prepare the planning graph out object
-                    tag_graph_out = PlanningGraphOut(
-                        data=graph_data,
-                        year1=this_year,
-                        year2=last_year,
-                        year1_avg=abs(sub_tag.current_year_avg),
-                        year2_avg=abs(sub_tag.last_year_avg),
-                        pretty_name=pretty_name,
-                        key_name=key_name,
-                    )
-                    sub_graph_data.append(tag_graph_out)
+                    sub_graph_data.append(planning_graph_out)
                 graph_object = PlanningGraphList(
                     title=title, data=sub_graph_data
                 )
