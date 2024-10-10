ccc435ca72b7110501db61c3d650cb9fe19defb5
John Adams <johnmadams96@gmail.com>
2024-10-10 12:20:00 -0400

    chore: chanve to version 1.0.042

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -52,7 +52,7 @@ const { prefetchVersion, version } = useVersion();
 const showBanner = ref(false);
 
 const checkVersion = computed(() => {
-  return version.value && version.value.version_number !== "1.0.041";
+  return version.value && version.value.version_number !== "1.0.042";
 });
 
 const updateBanner = () => {
--- frontend/src/views/AppNavigationVue.vue
+++ frontend/src/views/AppNavigationVue.vue
@@ -6,7 +6,7 @@
       </template>
       <v-app-bar-title>
         <span class="text-caption font-weight-bold"
-          >v1.0.041</span
+          >v1.0.042</span
         ></v-app-bar-title
       >
       <v-menu location="start">
