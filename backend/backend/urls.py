466380d240988f1c67d40d7955112534200018ae
John Adams <johnmadams96@gmail.com>
2024-10-11 10:32:15 -0400

    chore: change version 1.0.043

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -60,7 +60,7 @@ from planning.api.routers.planning_graph import planning_graph_router
 
 api = NinjaAPI(auth=GlobalAuth())
 api.title = "LenoreFin API"
-api.version = "1.0.042"
+api.version = "1.0.043"
 api.description = "API documentation for LenoreFin"
 
 # Add routers to the API
--- frontend/src/views/AppNavigationVue.vue
+++ frontend/src/views/AppNavigationVue.vue
@@ -6,7 +6,7 @@
       </template>
       <v-app-bar-title>
         <span class="text-caption font-weight-bold"
-          >v1.0.042</span
+          >v1.0.043</span
         ></v-app-bar-title
       >
       <v-menu location="start">
