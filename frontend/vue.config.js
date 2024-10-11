326a67a4050daa78a47ccb273f90fb6e89af9e7b
John Adams <johnmadams96@gmail.com>
2024-10-11 09:59:01 -0400

    fix: vue hydration errors

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,4 @@
+const webpack = require("webpack");
 module.exports = {
   chainWebpack: config => {
     config.plugin("html").tap(args => {
@@ -15,5 +16,14 @@ module.exports = {
     },
     allowedHosts: ["front-dev.danielleandjohn.love"],
     https: true,
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: JSON.stringify(true),
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+      }),
+    ],
   },
 };
