8ea7582da47665e4224ac612f7c4cda354ceb549
John Adams <johnmadams96@gmail.com>
2024-12-05 13:48:18 -0500

    chore: change to version 1.0.055

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -52,7 +52,7 @@ const { prefetchVersion, version } = useVersion();
 const showBanner = ref(false);
 
 const checkVersion = computed(() => {
-  return version.value && version.value.version_number !== "1.0.054";
+  return version.value && version.value.version_number !== "1.0.055";
 });
 
 const updateBanner = () => {
--- frontend/src/views/AppNavigationVue.vue
+++ frontend/src/views/AppNavigationVue.vue
@@ -6,7 +6,7 @@
       </template>
       <v-app-bar-title>
         <span class="text-caption font-weight-bold"
-          >v1.0.054</span
+          >v1.0.055</span
         ></v-app-bar-title
       >
       <v-menu location="start">
