8fee905a5199674109d8e21acdb1601046a9af29
John Adams <johnmadams96@gmail.com>
2025-07-14 13:03:38 -0400

    fix: missing api_key env variable

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -9,5 +9,8 @@ else
     ln -snf /usr/share/zoneinfo/UTC /etc/localtime && echo "UTC" >/etc/timezone
 fi
 
+# Load environment variables from .env file
+sed -i "s|__VITE_API_KEY__|${VITE_API_KEY}|g" /usr/share/nginx/html/index.html
+
 # Execute the main process
 exec "$@"
